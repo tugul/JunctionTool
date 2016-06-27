464a18c22864eb7b4a973455384fc33ff9e4c52c
tugul <tugul@live.com>
2016-06-27 21:44:34 +0200

    Initial commit

---
+++ JunctionManager/src/com/main/Entry.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.united.Service;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		Service service = new Service();
+		
+		System.out.println("Initiated");
+	}
+
+}
